9802cafefc4c785891dc3a4a6ff4929247fc19cf
Efrain Palencia <epalen6@wgu.edu>
2024-05-19 21:20:29 -0400

    Initial setup
    Creates the tutorial project along with the quickstart app. The settings.py file is set to accept environment variables.

---
+++ tutorial/tutorial/production.py
@@ -0,0 +1,8 @@
+from tutorial.settings import *
+
+DEBUG = False
+
+SECRET_KEY = 'django-insecure-!f#dz0!1gs$pdhqy^d_)jous5b#i7vs*2dl2u9%*&y_i$9&!@a'
+ALLOWED_HOSTS = []
+EMAIL_HOST = ''
+EMAIL_PORT = ''
